7bc384666cd706b3daf547aa2fc0a27fbfc46731
evgeniynd@gmail.com <evgeniynd@gmail.com>
2017-02-05 23:39:10 +0300

    ggjjoo

---
+++ BARC/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace BARC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Table1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.DataSet1 = new BARC.DataSet1();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.Table1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Table1BindingSource
+            // 
+            this.Table1BindingSource.DataMember = "Table1";
+            this.Table1BindingSource.DataSource = this.DataSet1;
+            // 
+            // DataSet1
+            // 
+            this.DataSet1.DataSetName = "DataSet1";
+            this.DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.Table1BindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "BARC.Report2.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(528, 545);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(528, 545);
+            this.Controls.Add(this.reportViewer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Конвертер QR кодов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Table1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource Table1BindingSource;
+        private DataSet1 DataSet1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
+
---
+++ BARC/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.QrCode;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Reporting.WinForms;
+
+
+
+
+namespace BARC
+{
+    public partial class Form1 : Form
+    {
+//        private DataSet1 data = new DataSet1();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] encoderu(string text, System.Drawing.Imaging.ImageFormat format)
+        {
+            System.Collections.Generic.Dictionary<EncodeHintType, object> param = new Dictionary<EncodeHintType, object>();
+            param.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
+            param.Add(EncodeHintType.MARGIN, 0);
+            param.Add(EncodeHintType.MIN_SIZE, 9);
+            ZXing.QrCode.QRCodeWriter q = new QRCodeWriter();
+            return ConvertToBitmapImage(q.encode(text, BarcodeFormat.QR_CODE, 1, 1, param), format);
+        }
+
+        private byte[] ConvertToBitmapImage(ZXing.Common.BitMatrix barcode, System.Drawing.Imaging.ImageFormat format)
+        {
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            BarcodeWriter bw = new BarcodeWriter();
+            Bitmap barcodeBitmap = bw.Write(barcode);
+            barcodeBitmap.Save(ms, format);
+            ms.Position = 0;
+            return ms.ToArray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Соединяемся с EXCEL файлом и создаем DataSet
+            OleDbConnection MyConnection;
+            OleDbDataAdapter MyCommand;
+            MyConnection = new OleDbConnection(@"provider=Microsoft.Jet.OLEDB.4.0;Data Source='db.xls';Extended Properties=Excel 8.0;");
+            MyCommand = new OleDbDataAdapter("select * from [Лист1$]", MyConnection);
+            MyCommand.Fill(this.DataSet1.Table1);
+            MyConnection.Close();
+            string code;
+            foreach (BARC.DataSet1.Table1Row row in this.DataSet1.Table1)
+            {
+                code = row.NAM.Trim();
+                try
+                {
+                    row.BARCODE = encoderu(code, System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+                catch (Exception err) 
+                {
+                    if (err == null) { };
+                }
+            }
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
